6cc59849ac47750879ab32fe4027a3d9069fc246
SharmaNik89 <163467746+SharmaNik89@users.noreply.github.com>
2024-03-16 17:17:22 -0700

    Add files via upload

---
+++ Medico Front-End/medicoApp/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes: Routes = [
+  {path : 'home' , component : HomeComponent},
+  { path : 'landingPage', component : LandingPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
